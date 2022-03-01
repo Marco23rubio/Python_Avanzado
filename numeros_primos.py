c1bb4983ff4785027cd8d13e499392a16a6de4d0
Marco Rubio <marcoantonio23rubio@gmail.com>
2022-03-01 12:58:55 -0700

    Termine la función de encontrar numeros primos

---
+++ numeros_primos.py
@@ -0,0 +1,17 @@
+def is_prime(a:int) -> bool:
+    if a == 1:
+        return False
+    elif a == 2:
+        return True
+    for i in range(2,a):
+        if a % i == 0:
+            return False
+        else:
+            return True
+        
+            
+def run():
+    print(is_prime(4))
+
+if __name__ == '__main__':
+    run()
