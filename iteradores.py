c27a86b2c6bc2f870ed0de33545d61db1af547a4
Marco Rubio <marcoantonio23rubio@gmail.com>
2022-03-07 14:57:38 -0700

    Se realizo la sucesión de fibonacci con iteradores

--- iteradores.py
+++ iteradores.py
@@ -1,4 +1,34 @@
-my_list = [1,2,3,4,5]
-my_iter = iter(my_list)
+import time
 
-print(next(my_iter))
+class Fiboiter():
+    def __init__(self,max=int):
+        self.max = max
+    
+    def __iter__(self):
+        self.n1 = 0
+        self.n2 = 1
+        self.count = 0
+        self.num = 0
+        return self
+    
+    def __next__(self):
+        if self.count == 0:
+            self.count += 1
+            return self.n1
+        elif self.count == 1:
+            self.count += 1
+            return self.n2
+        else:
+            self.num = self.n1 + self.n2
+            if self.num >= self.max:
+                raise StopIteration
+            self.n1 , self.n2 = self.n2 , self.num 
+            return self.num
+
+
+
+if __name__ == "__main__":
+    fibonacci = Fiboiter(5585)
+    for element in fibonacci:
+        print(element)
+        time.sleep(0.3)
