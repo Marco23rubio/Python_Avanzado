785d4ca3013af719d00acbdd97aef5c65bfc4112
Marco Rubio <marcoantonio23rubio@gmail.com>
2022-03-08 17:55:28 -0700

    Se creo una función para remover duplicados en una lista y despues convertir esa lista en un set

--- sets.py
+++ sets.py
@@ -8,16 +8,16 @@
 # my_set5 = my_set4 | my_set1
 # print(my_set5)
 
-def make_unique_list(funct):
-    no_duplicates = []
-    for element in funct:
-        if element not in no_duplicates:
-            no_duplicates.append(element)
-    return set(no_duplicates)
+#def make_unique_list(funct):
+    # no_duplicates = []
+    # for element in funct:
+    #     if element not in no_duplicates:
+    #         no_duplicates.append(element)
+    # return set(no_duplicates)
 
 def run():
     lista = [1,2,3,4,4,6,"r",7,5,"r","a","b","b"]
-    print(make_unique_list(lista))
+    print(set(lista))
 
 if __name__ == '__main__':
     run()
