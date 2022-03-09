31b6ff15baf9caa197bbd9ed5c6afbe93afb4ed7
Marco Rubio <marcoantonio23rubio@gmail.com>
2022-03-08 17:17:08 -0700

    Se realizo ejemplo básico de sets

---
+++ sets.py
@@ -0,0 +1,10 @@
+my_set1 ={"Carlos","Daniel","Abel","Cholo","Chino"}
+my_set2 ={"Marco","Carlos","Pedro","Juanito","Ines"}
+my_set3 ={"María","Mata","Mozart","Carlos","Santiago"}
+
+my_set4 = my_set1 & my_set2 & my_set3
+print(my_set4)
+
+my_set5 = my_set4 | my_set1
+print(my_set5)
+
